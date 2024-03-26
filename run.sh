a9261ad61c32faefa8a9d6c9fdc408a77eb74919
HoTaRV <89452877+HoTaRV@users.noreply.github.com>
2024-03-26 21:26:51 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-e91a7c46-2217-44a9-9732-9d64164c1594/)
 
 
 echo "$curl_output" >> Update.tsx
